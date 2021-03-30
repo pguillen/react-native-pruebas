99c3b909bd7a029fb65a72c192c318193daf26e9
pguillen <pguillen@gmail.com>
2021-03-30 19:48:13 -0300

    Pruebas con TextInput

--- App.js
+++ App.js
@@ -1,57 +1,36 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Dimensions } from "react-native";
 
-const Texto = ({ style }) => {
-  // const { texto } = props;
-  // return <Text> {texto} </Text>;
-
-  // const { children } = props;
-  // return <Text> {children} </Text>;
-
-  const [texto, setTexto] = useState("Hola mundo!");
-  const actualizaTexto = () => {
-    setTexto("Chao mundo!");
-  };
-  return (
-    <Text style={[styles.text, style]} onPress={actualizaTexto}>
-      {texto}
-    </Text>
-  );
-};
+const width = Dimensions.get('window').width
 
 export default function App() {
+
+  const [text, setText] = useState('Chanchito Feliz')
+
   return (
     <View style={styles.container}>
-      <Texto style={styles.red} />
-      <Texto style={styles.green} />
-      <Texto style={styles.blue} />
-      <StatusBar style="auto" />
+      <Text> Texto: {text} </Text>
+      <TextInput 
+        style={styles.input} 
+        placeholder='Escribe acá'
+        onChangeText= {t => setText(t)}
+        defaultValue={text}
+       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    color: "white",
-    fontSize: 24,
-    height: 100,
-    width: 100,
-  },
-  red: {
-    backgroundColor: "red",
-  },
-  green: {
-    backgroundColor: "green",
-  },
-  blue: {
-    backgroundColor: "blue",
+  input:{
+    height:40,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    width: width,
   },
   container: {
     flex: 1,
-    flexDirection: "column-reverse",
     backgroundColor: "#fff",
-    alignItems: "flex-start",
+    alignItems: "center",
     justifyContent: "space-evenly",
   },
 });
