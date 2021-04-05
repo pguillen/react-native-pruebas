9356b76b48d5679de2c9e593c17707c9b73c4c12
pguillen <pguillen@gmail.com>
2021-04-05 13:46:25 -0300

    Puntos en un Mapa

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
+import { Map, Modal, Panel, Input, List } from "./components";
 
 export default function App() {
+  const [puntos, setPuntos] = useState([]);
+  const [puntoTemp, setPuntoTemp] = useState({});
+  const [nombre, setNombre] = useState("");
+  const [visibilityFilter, setVisibilityFilter] = useState("new_punto");
+  const [visibility, setVisibility] = useState(false);
+  const [pointsFilter, setPointsFilter] = useState(false);
+
+  const togglePointsFilter = () => setPointsFilter(!pointsFilter);
+
+  const handleLoongPress = (e) => {
+    setPuntoTemp(e.nativeEvent.coordinate);
+    setVisibilityFilter("new_punto");
+    setVisibility(true);
+  };
+
+  const handleChangeText = (text) => {
+    setNombre(text);
+  };
+
+  const handlerSubmit = () => {
+    const newPunto = { coordinate: puntoTemp, name: nombre };
+    setPuntos(puntos.concat(newPunto));
+    setVisibility(false);
+    setNombre("");
+  };
+
+  const handlerList = () => {
+    setVisibilityFilter("all_puntos");
+    setVisibility(true);
+  };
+
+  console.log(puntos);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Map
+        onLongPress={handleLoongPress}
+        puntos={puntos}
+        pointsFilter={pointsFilter}
+      />
+      <Panel
+        onPressLeft={handlerList}
+        textLeft="Lista"
+        togglePointsFilter={togglePointsFilter}
+      />
+      <Modal visibility={visibility}>
+        {visibilityFilter === "new_punto" ? (
+          <View style={styles.form}>
+            <Input
+              title="Nombre"
+              placeholder="Nombre del punto"
+              onChangeText={handleChangeText}
+            />
+            <Button title="Aceptar" onPress={handlerSubmit} />
+          </View>
+        ) : (
+          <List puntos={puntos} closeModal={() => setVisibility(false)} />
+        )}
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  form: {
+    padding: 15,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
