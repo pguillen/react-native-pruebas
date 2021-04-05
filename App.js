f2fecef2f1317da817d952a7903a4b0831764c89
pguillen <pguillen@gmail.com>
2021-04-05 19:55:56 -0300

    Mostrar imagenes, indicador de actividad y imagenes de fondo

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  Image,
+  ImageBackground,
+} from "react-native";
 
 export default function App() {
   const [users, setUsers] = useState([]);
@@ -19,21 +27,39 @@ export default function App() {
     return (
       <View style={styles.center}>
         <Text>Cargando...</Text>
+        <ActivityIndicator size="small" color="#0000ff" />
       </View>
     );
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={users}
-        renderItem={({ item }) => <Text style={styles.item}> {item.name} </Text>}
-        keyExtractor={(item) => String(item.id)}
-      />
+      <Image style={styles.photoPic} source={require("./assets/icon.png")} />
+
+      <ImageBackground
+        style={styles.photo}
+        source={{ uri: "http://placekitten.com/500/300" }}
+      >
+        <FlatList
+          data={users}
+          renderItem={({ item }) => (
+            <Text style={styles.item}> {item.name} </Text>
+          )}
+          keyExtractor={(item) => String(item.id)}
+        />
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  photo: {
+    height: 500,
+    width: 300,
+  },
+  photoPic: {
+    height: 60,
+    width: 60,
+  },
   center: {
     flex: 1,
     alignItems: "center",
