907fd62ebb96b278a22c3ee8065dc00ea6cbd6e4
Pablo <pguillen@gmail.com>
2021-04-06 20:41:31 -0300

    Agergo boton redondo para voltear la camara

--- 3_AAH_Camara/App.js
+++ 3_AAH_Camara/App.js
@@ -6,6 +6,7 @@ import {
   Text,
   View,
   Button,
+  TouchableOpacity,
 } from "react-native";
 
 import { Camera } from "expo-camera";
@@ -42,21 +43,24 @@ export default function App() {
   } else {
     return (
       <View style={styles.container}>
-        <Button
-          style={styles.roundButton}
-          title="Voltear"
-          onPress={() => {
-            const { front, back } = Camera.Constants.Type;
-            const nuevoTipo = tipo === back ? front : back;
-            setTipo(nuevoTipo);
-          }}
-        />
-
         <Camera style={styles.camera} type={tipo}>
           <View style={styles.fragment}>
             <Text style={styles.text}>Acceso Al Hardware </Text>
             <Text style={styles.text}>Este es mi camara</Text>
           </View>
+          <TouchableOpacity
+            style={styles.roundButton}
+            title="Voltear"
+            onPress={() => {
+              const { front, back } = Camera.Constants.Type;
+              const nuevoTipo = tipo === back ? front : back;
+              setTipo(nuevoTipo);
+            }}
+          >
+            <View style={styles.fragment}>
+              <Text style={styles.text}>Voltear</Text>
+            </View>
+          </TouchableOpacity>
         </Camera>
       </View>
     );
@@ -70,21 +74,19 @@ const styles = StyleSheet.create({
     height: Dimensions.get("window").height,
   },
   roundButton: {
-    marginRight: 40,
-    marginLeft: 40,
-    marginTop: 10,
-    paddingTop: 20,
-    paddingBottom: 20,
-    backgroundColor: "#68a0cf",
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "#ffee80",
+    backgroundColor: "rgba(20,174,255,0.51)",
+    justifyContent: "center",
+    alignContent: "center",
+    borderWidth: 3,
+    borderRadius: 150,
+    width: 150,
+    height: 150,
   },
   fragment: {
     flex: 1,
     alignItems: "center",
-    paddingTop: 10,
-    borderWidth: 1,
+    justifyContent: "center",
+    alignContent: "center",
   },
   text: {
     fontSize: 22,
